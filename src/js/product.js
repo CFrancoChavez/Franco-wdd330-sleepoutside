45227b21d62422a2fa1ca7ab7a79246b22e2b749
CFrancoChavez <franco.chvez@gmail.com>
2025-09-23 08:59:03 -0300

    W02: Dynamic product pages and URL params

--- src/js/product.js
+++ src/js/product.js
@@ -1,28 +1,37 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+// import { getLocalStorage, setLocalStorage } from './utils.mjs';
+// import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData('tents');
+// const dataSource = new ProductData('tents');
 
+// // function addProductToCart(product) {
+// //   setLocalStorage('so-cart', product);
+// // }
 // function addProductToCart(product) {
-//   setLocalStorage('so-cart', product);
+//   // 1. Obtiene la lista actual del localStorage
+//   const cartItems = getLocalStorage('so-cart') || [];
+  
+//   // 2. Agrega el nuevo producto a la lista
+//   cartItems.push(product);
+  
+//   // 3. Guarda la lista completa y actualizada en localStorage
+//   setLocalStorage('so-cart', cartItems);
 // }
-function addProductToCart(product) {
-  // 1. Obtiene la lista actual del localStorage
-  const cartItems = getLocalStorage('so-cart') || [];
-  
-  // 2. Agrega el nuevo producto a la lista
-  cartItems.push(product);
-  
-  // 3. Guarda la lista completa y actualizada en localStorage
-  setLocalStorage('so-cart', cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById('addToCart')
+//   .addEventListener('click', addToCartHandler);
+import { getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
+
+const productId = getParam('product');
+const dataSource = new ProductData('tents');
+const product = new ProductDetails(productId, dataSource);
+
+product.init();
